19e7c589120a5466d420ecfb75ef7bc87f5d20da
data-en <scientistdata953@gmail.com>
2024-03-31 18:04:27 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # gives 3 important information from which first two we don't require
@@ -19,4 +19,3 @@ class CustomException(Exception):
         return self.error_message
     
  
-        
